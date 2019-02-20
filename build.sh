4948652d01cad78257ea1773473f89f3ff49dc5b
Philippe Creytens <philippe@neuronation.de>
2019-02-20 15:13:49 +0100

    enhanced script and csproj

--- build.sh
+++ build.sh
@@ -9,7 +9,18 @@ cd ../
 echo "replace so files for android monogame"
 cp -rf "MonoGame.Framework.Android.csproj" "MonoGame/MonoGame.Framework/MonoGame.Framework.Android.csproj"
 
+echo "get latest version of the exercise engine"
+cd app_c_sharp_exercise_engine/
+git pull
+cd ../
+
 echo "run ios project"
 msbuild CocosSharp.iOS.sln /p:Platform=iPhone /p:Configuration=Release
 echo "run android project"
 msbuild CocosSharp.Android.sln /p:Configuration=Release
+
+echo "push the new cocosengine"
+cd app_c_sharp_exercise_engine
+git add --all
+git commit -m "Cocosengine update"
+git push
