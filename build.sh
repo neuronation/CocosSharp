ce7733f32f742a5284c7df95aef907e79193aada
Philippe Creytens <philippe@neuronation.de>
2019-02-20 15:29:38 +0100

    Cocosengine update

--- build.sh
+++ build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 echo "update all submodules"
-git submodule update --init
+#git submodule update --init
 echo "create buildable monogame projects"
-cd Monogame/
-mono Protobuild.exe
-cd ../
+#cd Monogame/
+#mono Protobuild.exe
+#cd ../
 echo "replace so files for android monogame"
-cp -rf "MonoGame.Framework.Android.csproj" "MonoGame/MonoGame.Framework/MonoGame.Framework.Android.csproj"
+#cp -rf "MonoGame.Framework.Android.csproj" "MonoGame/MonoGame.Framework/MonoGame.Framework.Android.csproj"
 
 echo "get latest version of the exercise engine"
-cd app_c_sharp_exercise_engine/
-git checkout master
-git pull
-cd ../
+#cd app_c_sharp_exercise_engine/
+#git checkout master
+#git pull
+#cd ../
 
+echo "run PCL project"
+msbuild PCL/CocosSharp.PCL.sln /p:Configuration=Release
 echo "run ios project"
-msbuild CocosSharp.iOS.sln /p:Platform=iPhone /p:Configuration=Release
+#msbuild CocosSharp.iOS.sln /p:Platform=iPhone /p:Configuration=Release
 echo "run android project"
-msbuild CocosSharp.Android.sln /p:Configuration=Release
+#msbuild CocosSharp.Android.sln /p:Configuration=Release
 
 echo "push the new cocosengine"
 cd app_c_sharp_exercise_engine
